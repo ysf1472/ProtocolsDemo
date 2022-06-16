a92a12bb96c1aeadcc5e809afeff4bdb6b65757e
ysf1472 <yusuf.ahmet.nihat@gmail.com>
2022-06-16 00:39:40 -0400

    Initial Commit

---
+++ ProtocolsDemo/main.swift
@@ -0,0 +1,60 @@
+protocol canFly {
+    func fly()
+}
+class Bird  {
+    var isFemale = true
+    func layEgg() {
+        if isFemale{
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+   
+}
+class Eagle : Bird , canFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    func soar(){
+        print("The eagle glides in the air using air currents.")
+    }
+}
+class Penguin : Bird {
+    func swim(){
+        print("The penguin can paddles through the water.")
+    }
+}
+class Engine{
+    func accelerate(){
+        print("Engine starts to warm up!")
+    }
+}
+class Plane :Engine ,canFly{
+    func fly(){
+        print("Engine starts to warm up! to fly ")
+    }
+}
+struct Incest : canFly{
+    func fly() {
+        print("A Bug can fly as well!")
+    }
+    
+}
+struct flyingMuseum{
+    func flyingDemo(flyingObject : canFly){
+        flyingObject.fly()
+    }
+}
+let myEagle = Eagle()
+myEagle.fly()
+myEagle.layEgg()
+myEagle.soar()
+let penguin = Penguin()
+penguin.swim()
+let myplane = Plane()
+myplane.fly()
+myplane.accelerate()
+let bugs = Incest()
+bugs.fly()
+let museum = flyingMuseum()
+museum.flyingDemo(flyingObject: bugs)
